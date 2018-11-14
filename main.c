e0e5a1e2cfa1752768eadde29e3f7c2ddf5fc59f
Marc Antoine Dumont <dumont.marcantoine13@gmail.com>
2018-11-14 10:08:15 -0500

    Ajout des fonction a implementer

--- main.c
+++ main.c
@@ -1,4 +1,54 @@
-#include <stdio.h>
+#include "main.h"
+
+
+void initmem(int size_memory){
+
+
+}
+
+int allouem(int size_memory){
+
+
+
+	return 0;
+}
+
+int nbloclibres(){
+
+	return 0;
+}
+
+int nblocalloues(){
+
+
+	return 0;
+}
+
+
+int memlibre(){
+
+
+	return 0;
+}
+
+
+int mem_pgrand_libre(){
+
+
+	return 0;
+
+}
+
+int mem_small_free(int max_taille_petit){
+
+	return 0;
+}
+
+
+int mem_est_alloue(int pOctet){
+
+	return 0;
+}
 
 int main() {
     printf("Hello, World!\n");
---
+++ main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void initmem(int size_memory);
+int allouem(int size_memory);
+int nbloclibres();
+int nblocalloues();
+int memlibre();
+int mem_pgrand_libre();
+int mem_small_free(int max_taille_petit);
+int mem_est_alloue(int pOctet);
