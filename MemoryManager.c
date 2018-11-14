b642bd6cd98e20bb300a82209879b1bb05eccdca
tgattaz <thomas.gattaz@gmail.com>
2018-11-14 09:54:39 -0500

    Update MemoryManager.c

---
+++ MemoryManager.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bloc {
+	int size;
+	int offset;
+	void *data;
+} bloc;
+
+struct node
+{
+    int data;
+  	struct bloc *valeur;
+    struct node *p_next;
+    struct node *p_prev;
+};
+
+typedef struct dlist
+{
+    size_t length;
+    struct node *p_tail;
+    struct node *p_head;
+} Dlist;
+
+Dlist *list = NULL; /* Déclaration d'une liste vide */
+
+int main() {
+
+}
+
+void initmem(int size) {
+  struct bloc *bloc = malloc(sizeof(bloc));
+	bloc->size = memSize;
+	bloc->offset = 0;
+	bloc->data = NULL;
+
+	root = malloc(sizeof(node));
+	root->value = bloc;
+	root->previous = NULL;
+	root->next = NULL;
+}
