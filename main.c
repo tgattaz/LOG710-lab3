111bb323d8ae10f372176fbf33b96a76bc8b3d6b
tgattaz <thomas.gattaz@gmail.com>
2018-11-14 10:21:26 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct bloc {
+	int size;
+	int offset; //adresse
+	void *data;
+} bloc;
+
+struct node
+{
+    int data;
+  	struct bloc *valeur;
+    struct node *p_next;
+    struct node *p_prev;
+};
+
+struct node *root = NULL;
 
 void initmem(int size_memory){
+  struct bloc *bloc = malloc(sizeof(bloc));
+	bloc->size = memSize;
+	bloc->offset = 0;
+	bloc->data = NULL;
 
+	root = malloc(sizeof(node));
+	root->value = bloc;
+	root->previous = NULL;
+	root->next = NULL;
 
 }
 
