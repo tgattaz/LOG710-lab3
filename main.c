a261fb2d2b2ec14c09e89017360109c9036f4085
Lucas Bacle <lucas.bacle@free.fr>
2018-11-21 12:09:03 -0500

    Commit inutile : le retour.

--- main.c
+++ main.c
@@ -52,13 +52,13 @@ node *allou_mem(int new_block_size, node *free_node) {
 }
 
 node *libere_mem(node *lib_node) {
-    
+
     lib_node->value->state = 0;
 
     node *prev_node = lib_node->p_prev;
     node *next_node = lib_node->p_next;
 
-    if(prev_node->value->state == 0){
+    if (prev_node->value->state == 0) {
         lib_node->value->address = prev_node->value->address;
         lib_node->value->size += prev_node->value->size;
 
@@ -66,7 +66,7 @@ node *libere_mem(node *lib_node) {
         lib_node->p_prev->p_next = lib_node;
         free(prev_node);
     }
-    if(next_node->value->state == 0){
+    if (next_node->value->state == 0) {
         lib_node->value->size += next_node->value->size;
 
         lib_node->p_next = next_node->p_next;
@@ -197,9 +197,9 @@ int mem_est_alloue(node *memory_root, int pOctet) {
 }
 
 
-void affiche_etat_memoire(node *memory_root){
+void affiche_etat_memoire(node *memory_root) {
     node *p_mem = memory_root;
-     int n_memory_blocks = 0;
+    int n_memory_blocks = 0;
     printf("Liste des blocs memoires : \n");
 
     while (p_mem != NULL) {
@@ -215,12 +215,12 @@ void affiche_etat_memoire(node *memory_root){
 
 }
 
-void affiche_parametres_memoire(node *root){
+void affiche_parametres_memoire(node *root) {
 
     printf("Nombre de blocs libres: %u\n", n_bloc_libres(root));
     printf("Nombre de blocs alloués: %u\n", n_bloc_alloues(root));
     printf("Mémoire libre: %u\n", mem_libre(root));
-    printf("Taille du plus grand bloc libre: %u \n", mem_pgrand_libre(root)); 
+    printf("Taille du plus grand bloc libre: %u \n", mem_pgrand_libre(root));
 
     int size_small = 10;
     printf("Nombre de blocs libres de taille inférieur à %u: %u \n", size_small, mem_small_free(root, size_small));
